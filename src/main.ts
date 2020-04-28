7a356a2d1f9609636c6ab7a5aaf463d02b10b0e4
qrinef <qrinef@gmail.com>
2020-04-28 08:19:22 +0300

    fix: cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: ['http://localhost:3000', 'https://crm-backend-nuxt.qrinef.ru'],
     credentials: true,
   });
   await app.listen(3010);

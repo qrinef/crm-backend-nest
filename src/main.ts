de43b29512a72d3608bd5c6efbf881f97a9239f7
qrinef <qrinef@gmail.com>
2020-04-28 08:13:41 +0300

    fix: cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true
+    origin: true,
+    credentials: true,
   });
   await app.listen(3010);
 }
